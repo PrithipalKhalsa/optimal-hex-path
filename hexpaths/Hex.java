5594010ae6df19fa0121d050a65c3e2624a18e7a
PrithipalKhalsa <prithipalpsk@gmail.com>
2018-05-02 13:43:40 -0700

    Hex object works
    I added a print values to help us.

---
+++ hexpaths/Hex.java
@@ -0,0 +1,21 @@
+package algorithm;
+public class Hex implements Comparable<Hex> {
+    int current;
+    int position;
+
+
+    public Hex(int position, int current) {
+         this.position = position;
+         this.current = current;
+
+     }
+     public int printvalues(){
+       System.out.print(this.position+" ");
+       System.out.println(this.current);
+       return 0;
+     }
+     @Override
+         public int compareTo(Hex o) {
+             return 0;
+         }
+}
